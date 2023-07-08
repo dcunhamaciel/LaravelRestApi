3cf745f8b78af1cd92ebaa3be14c68eccd3063ee
dcunhamaciel <dcunhamaciel@gmail.com>
2023-07-08 11:23:46 -0300

    Criando Rota de Teste

--- 01.primeira_api/routes/api.php
+++ 01.primeira_api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/test', function(Request $request) {
+    //dd($request->headers->all());
+    //dd($request->headers->get('Authorization'));
+
+    $response = new Response(json_encode(['msg' => 'Minha primeira resposta de API']));
+    $response->header('Content-Type', 'application/json');
+
+    return $response;
 });
