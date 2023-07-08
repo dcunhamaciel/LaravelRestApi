1ae323567710e3ce6300825c5cbc48f6fd38484a
dcunhamaciel <dcunhamaciel@gmail.com>
2023-07-08 12:15:44 -0300

    Editando Produto

--- 01.primeira_api/routes/api.php
+++ 01.primeira_api/routes/api.php
@@ -34,4 +34,5 @@ Route::prefix('products')->group(function () {
     Route::get('/', [ProductController::class, 'index']);
     Route::get('/{id}', [ProductController::class, 'show']);
     Route::post('/', [ProductController::class, 'save']);
+    Route::put('/', [ProductController::class, 'update']);
 });
