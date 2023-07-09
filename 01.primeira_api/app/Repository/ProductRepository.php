c52ba87dff3c5fe29f998860e4bc674bed50f436
dcunhamaciel <dcunhamaciel@gmail.com>
2023-07-09 11:13:52 -0300

    Criando AbstractRepository

--- 01.primeira_api/app/Repository/ProductRepository.php
+++ 01.primeira_api/app/Repository/ProductRepository.php
@@ -2,38 +2,6 @@
 
 namespace App\Repository;
 
-use Illuminate\Database\Eloquent\Model;
-
-class ProductRepository
+class ProductRepository extends AbstractRepository
 {
-    /**
-     * @var Model
-     */
-    private $model;
-
-    public function __construct($model)
-    {
-        $this->model = $model;
-    }
-
-    public function selectFilter($columns) 
-    {   
-        $this->model = $this->model->selectRaw($columns);
-    }
-
-    public function selectConditions($conditions)
-    {
-        $expressions = explode(';', $conditions);
-
-        foreach($expressions as $expression) {
-            $exp = explode(':', $expression);
-            
-            $this->model = $this->model->where($exp[0], $exp[1], $exp[2]);
-        }
-    }
-
-    public function getResult()
-    {
-        return $this->model;
-    }
 }
