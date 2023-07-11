434582efbe5d2a0c01b9ca891fab4952a177daa3
dcunhamaciel <dcunhamaciel@gmail.com>
2023-07-11 20:40:07 -0300

    Endpoint para Atualização de um Imóvel

--- 02.meu_imovel/app/Http/Controllers/Api/RealStateController.php
+++ 02.meu_imovel/app/Http/Controllers/Api/RealStateController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\RealState;
+use Ramsey\Uuid\Type\Integer;
 
 class RealStateController extends Controller
 {
@@ -31,7 +32,25 @@ class RealStateController extends Controller
 
             return response()->json([
                 'data' => [
-                    'msg' => 'Imóvel cadastrado co sucesso!'
+                    'msg' => 'Imóvel cadastrado com sucesso!'
+                ]
+            ]);
+        } catch(\Exception $error) {
+            return response()->json(['error' => $error->getMessage()], 401);
+        }
+    }
+
+    public function update(Request $request, int $id)
+    {
+        $data = $request->all();
+
+        try {
+            $realState = $this->realState->findOrFail($id);
+            $realState->update($data);
+
+            return response()->json([
+                'data' => [
+                    'msg' => 'Imóvel atualizado com sucesso!'
                 ]
             ]);
         } catch(\Exception $error) {
