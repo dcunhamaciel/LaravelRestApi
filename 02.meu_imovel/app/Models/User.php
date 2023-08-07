7ac72d6e704e4ff78b3a50478068a282db852153
dcunhamaciel <dcunhamaciel@gmail.com>
2023-08-07 19:54:36 -0300

    Configurando User e Driver JWT

--- 02.meu_imovel/app/Models/User.php
+++ 02.meu_imovel/app/Models/User.php
@@ -7,8 +7,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Tymon\JWTAuth\Contracts\Providers\JWT;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -51,5 +53,15 @@ class User extends Authenticatable
     public function realStates()
     {
         return $this->hasMany(RealState::class);
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();        
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
