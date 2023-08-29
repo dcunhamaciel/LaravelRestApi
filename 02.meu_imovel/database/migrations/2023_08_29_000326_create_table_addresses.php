a6699f1393594e440413ae997bb9064d4d4d8dad
dcunhamaciel <dcunhamaciel@gmail.com>
2023-08-28 21:11:05 -0300

    Migration de Endereço

---
+++ 02.meu_imovel/database/migrations/2023_08_29_000326_create_table_addresses.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('state_id');
+            $table->unsignedBigInteger('city_id');
+            $table->string('address');
+            $table->integer('number');
+            $table->string('neighborhood');
+            $table->string('complement')->nullable();
+            $table->integer('zip_code');
+            $table->timestamps();
+
+            $table->foreign('state_id')->references('id')->on('states');
+            $table->foreign('city_id')->references('id')->on('cities');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
